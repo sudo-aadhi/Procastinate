af39c33c5290be30f5d57fbb2bc0e1234580b927
sudo-aadhi <sudoaadhi@gmail.com>
2024-11-08 11:54:12 +0530

    Window container component added to codebase

---
+++ app/components/WindowContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WindowContainer = () => {
+  return (
+    <div className="flex justify-center items-center bg-[#0A0A0A] text-center w-[865px] h-[583.02px] border border-[#242323] rounded-[15px] absolute top-[600px]">
+      <div className="flex justify-center items-center bg-[#0E0E0E] text-center w-[836px] h-[556px] border border-[#242323] rounded-[11px]"></div>
+    </div>
+  );
+};
+
+export default WindowContainer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import WindowContainer from "./components/WindowContainer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center w-full min-h-screen bg-[#030303]">
+    <div className="flex flex-col items-center w-full h-[3500px] bg-[#030303]">
       <Hero />
+      <WindowContainer />
     </div>
   );
 }
